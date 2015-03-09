02a791502891f2d1f21f3f726da7b0459354e220
damjosu <damjosu@gmail.com>
2015-03-09 09:11:13 +0100

    primer commit

---
+++ NameGenerator.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class NameGenerator here.
+ * 
+ * @author (Josu) 
+ * @version 9.3.15 (09-03-2015)
+ */
+public class NameGenerator
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class NameGenerator
+     */
+    public NameGenerator()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
