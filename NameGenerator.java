e657cb37950a4ccade6b91296c5d512df0e6f63e
damjosu <damjosu@gmail.com>
2015-03-09 09:46:13 +0100

    método para generar nombre de Star Wars creado.

--- NameGenerator.java
+++ NameGenerator.java
@@ -10,6 +10,15 @@ public class NameGenerator
     private String primerApellido;
     private String segundoApellido;
     private String ciudad;
+    private static final int LETRAS_PRIMER_APELLIDO_INICIAL = 0;
+    private static final int LETRAS_PRIMER_APELLIDO_FINAL = 2;
+    private static final int LETRAS_NOMBRE_INICIAL = 0;
+    private static final int LETRAS_NOMBRE_FINAL = 2;
+    private static final int LETRAS_SEGUNDO_APELLIDO_INICIAL = 0;
+    private static final int LETRAS_SEGUNDO_APELLIDO_FINAL = 1;
+    private static final int LETRAS_CIUDAD_INICIAL = 0;
+    private static final int LETRAS_CIUDAD = 2;
+    
 
     /**
      * Constructor for objects of class NameGenerator
@@ -21,8 +30,14 @@ public class NameGenerator
         this.ciudad= ciudad;
     }
     
-    public String generateStarWarsName() {
-        String nombreStarWars = null;
-        return nombreStarWars;
+    /**
+     * Genera tu nombre en el universo de Star Wars dandole tu nombre, apellido, segundo apellido y ciudad.
+     */
+    public void generateStarWarsName() {
+        String nombreStarWars = nombre.substring(LETRAS_PRIMER_APELLIDO_INICIAL, LETRAS_PRIMER_APELLIDO_FINAL)
+        + nombre.substring(LETRAS_NOMBRE_INICIAL, LETRAS_NOMBRE_FINAL);
+        String apellidoStarWars = segundoApellido.substring(LETRAS_SEGUNDO_APELLIDO_INICIAL, LETRAS_SEGUNDO_APELLIDO_FINAL) 
+        + ciudad.substring(LETRAS_CIUDAD_INICIAL, LETRAS_CIUDAD);
+        System.out.println("Tu nombre en el universo de Star Wars sería: " + nombreStarWars + " " + apellidoStarWars);
     }
 }
