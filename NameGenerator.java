4eca8285167abfc4b0f4e73d21b93fde121d956f
damjosu <damjosu@gmail.com>
2015-03-09 09:19:51 +0100

    atributos y constructor creados

--- NameGenerator.java
+++ NameGenerator.java
@@ -1,33 +1,28 @@
 
 /**
- * Write a description of class NameGenerator here.
- * 
  * @author (Josu) 
  * @version 9.3.15 (09-03-2015)
  */
 public class NameGenerator
 {
-    // instance variables - replace the example below with your own
-    private int x;
+    
+    private String nombre;
+    private String primerApellido;
+    private String segundoApellido;
+    private String ciudad;
 
     /**
      * Constructor for objects of class NameGenerator
      */
-    public NameGenerator()
-    {
-        // initialise instance variables
-        x = 0;
+    public NameGenerator(String nombre, String primerApellido, String segundoApellido, String ciudad) {
+        this.nombre = nombre;
+        this.primerApellido = primerApellido;
+        this.segundoApellido = segundoApellido;
+        this.ciudad= ciudad;
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    
+    public String generateStarWarsName() {
+        String nombreStarWars = null;
+        return nombreStarWars;
     }
 }
